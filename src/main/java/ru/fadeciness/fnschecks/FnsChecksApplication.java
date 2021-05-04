8c08bcda69d555d5783267793a3a357318904b77
Evgeniy <solodimov.eugene@yandex.ru>
2021-05-04 22:26:56 +0300

    Initial commit

---
+++ src/main/java/ru/fadeciness/fnschecks/FnsChecksApplication.java
@@ -0,0 +1,13 @@
+package ru.fadeciness.fnschecks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnsChecksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnsChecksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fadeciness/fnschecks/FnsChecksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fadeciness.fnschecks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnsChecksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
